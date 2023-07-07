9dee512e267a333ae67aa89ad2b3020fe7378128
GIGABYTE <GIGABYTE@DESKTOP-DV1NCG8>
2023-07-08 01:21:53 +0800

    0708

--- my/src/app/Client.java
+++ my/src/app/Client.java
@@ -3,6 +3,10 @@ package app;
 import java.io.*;
 import java.net.*;
 import javax.swing.*;
+
+import clas.MyDrawerV2;
+
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -11,6 +15,8 @@ public class Client extends JFrame {
 	private JTextField usernameField; // 輸入使用者名稱的文本框
 	private JPasswordField passwordField; // 輸入密碼的密碼框
 	private JButton submitButton; // 提交按鈕
+	private MyDrawerV2 o;
+
 	private Socket socket; // 用於與伺服器建立連接的Socket對象
 	private PrintWriter writer; // 用於向伺服器發送數據的PrintWriter對象
 	private BufferedReader serverReader; // 用於從伺服器接收數據的BufferedReader對象
@@ -19,22 +25,28 @@ public class Client extends JFrame {
 		super("入口"); // 設定窗口標題
 		usernameField = new JTextField(20); // 創建一個寬度為20的文本框來輸入使用者名稱
 		passwordField = new JPasswordField(20); // 創建一個寬度為20的密碼框來輸入密碼
-		result = new JLabel(); // 創建一個用於顯示結果的標籤
+		result = new JLabel("輸入帳密"); // 創建一個用於顯示結果的標籤
 		submitButton = new JButton("送出"); // 創建一個標籤為"送出"的按鈕
-		setLayout(new FlowLayout()); // 使用流式布局
-		add(new JLabel("帳號：")); // 添加一個標籤，顯示"帳號："
-		add(usernameField); // 添加使用者名稱文本框
-		add(new JLabel("密碼：")); // 添加一個標籤，顯示"密碼："
-		add(passwordField); // 添加密碼框
-		add(result); // 添加結果標籤
-		add(submitButton); // 添加提交按鈕
+		JPanel top = new JPanel(new FlowLayout()); // 使用流式布局
+		top.add(new JLabel("帳號：")); // 添加一個標籤，顯示"帳號："
+		top.add(usernameField); // 添加使用者名稱文本框
+		top.add(new JLabel("密碼：")); // 添加一個標籤，顯示"密碼："
+		top.add(passwordField); // 添加密碼框
+		top.add(result);
+		top.add(submitButton); // 添加提交按鈕
+		
+		add(top, BorderLayout.CENTER);
+		
 		setDefaultCloseOperation(EXIT_ON_CLOSE); // 設定關閉窗口時的默認操作
-		setSize(300, 150); // 設定窗口大小為300x150
+		setSize(265, 150); // 設定窗口大小為300x150
 		setVisible(true); // 顯示窗口
+		o = new MyDrawerV2();
 		submitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (sendData()) {
-					setSize(640, 480); // 如果數據發送成功，則將窗口大小設定為640x480
+				top.removeAll();
+				add(o, BorderLayout.CENTER);
+				setSize(750, 750); // 如果數據發送成功，則將窗口大小設定為640x480
 				}
 			}
 		});
@@ -50,12 +62,17 @@ public class Client extends JFrame {
 	private boolean sendData() {
 		String username = usernameField.getText(); // 獲取使用者名稱文本框的內容
 		String password = new String(passwordField.getPassword()); // 獲取密碼框的內容
+		//if(username.equals("")||password.equals("")) {
+//			result.setText("不能空白");
+//			return false;
+		//}
 		writer.println(username); // 向伺服器發送使用者名稱
 		writer.println(password); // 向伺服器發送密碼
 		String response = null;
 		try {
 			response = serverReader.readLine(); // 從伺服器讀取響應
 			result.setText(String.format(response)); // 在結果標籤中顯示響應
+			
 		} catch (Exception e) {
 			System.out.println(e); // 如果發生異常，則輸出異常信息
 		}
--- my/src/clas/MyDrawerV2.java
+++ my/src/clas/MyDrawerV2.java
@@ -24,143 +24,172 @@ public class MyDrawerV2 extends JPanel {
 	private LinkedList<Line> lines, garbag;
 	private Color nowColor;
 	private float nowWidth;
-	
+
 	public MyDrawerV2() {
 		lines = new LinkedList<>();
 		garbag = new LinkedList<>();
-		setBackground(Color.YELLOW);
+		// setBackground(Color.YELLOW);
 		MyListener myListener = new MyListener();
 		addMouseListener(myListener);
 		addMouseMotionListener(myListener);
-		
+
 		nowColor = Color.BLUE;
 		nowWidth = 4;
-		
+
 	}
-	
-	public void setColor(Color newColor) {nowColor = newColor;}
-	public Color getColor() {return nowColor;}
-	public void setWidth (float newWidth) {nowWidth = newWidth;}
-	
+
+	public void setColor(Color newColor) {
+		nowColor = newColor;
+	}
+
+	public Color getColor() {
+		return nowColor;
+	}
+
+	public void setWidth(float newWidth) {
+		nowWidth = newWidth;
+	}
+
 	private class MyListener extends MouseAdapter {
 		@Override
 		public void mousePressed(MouseEvent e) {
 			int x = e.getX(), y = e.getY();
-			
-			Line line = new Line(nowColor, nowWidth);
-			line.addPoint(x, y);
-			
-			lines.add(line);
-			
-			garbag.clear();
+			Line line = new Line(nowColor, nowWidth);// 加line
+			if (boom(x, y)) {
+				line.addPoint(x, y);// 加點
+				lines.add(line);// lines+line
+			}
 		}
+
 		@Override
 		public void mouseDragged(MouseEvent e) {
 			int x = e.getX(), y = e.getY();
-			lines.getLast().addPoint(x, y);
-			
-			repaint();
+			if (boom(x, y)) {
+				lines.getLast().addPoint(x, y);
+				repaint();
+				//System.out.println(lines.getLast().getSize());
+			}
+
+		}
+
+		public void mouseReleased(MouseEvent e) {
+
 		}
 	}
-	
+
+	public boolean boom(int mouseX, int mouseY) {
+		int a = mouseX - 375, b = mouseY - 375;
+		double L = Math.pow((Math.pow(a, 2) + Math.pow(b, 2)), 0.5);
+		if (L > 225 && L < 240) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D)g;
-		
+		Graphics2D g2d = (Graphics2D) g;
+		g.drawOval(150, 150, 450, 450);
+		g.drawOval(135, 135, 480, 480);
+
 		for (Line line : lines) {
 			g2d.setColor(line.getColor());
 			g2d.setStroke(new BasicStroke(line.getWidth()));
-			
-			for (int i=1; i<line.getSize(); i++) {
-				g2d.drawLine(line.getPointX(i-1), line.getPointY(i-1), 
-						line.getPointX(i), line.getPointY(i));
+
+			for (int i = 1; i < line.getSize(); i++) {
+				
+				g2d.drawLine(line.getPointX(i - 1), line.getPointY(i - 1), line.getPointX(i), line.getPointY(i));
+
 			}
-			
 		}
 	}
-	
+
 	public void clear() {
 		lines.clear();
 		garbag.clear();
 		repaint();
 	}
-	
+
 	public void undo() {
-		if (lines.size()>0) {
+		if (lines.size() > 0) {
 			garbag.add(lines.removeLast());
 			repaint();
 		}
 	}
-	
+
 	public void redo() {
 		if (garbag.size() > 0) {
 			lines.add(garbag.removeLast());
 			repaint();
 		}
 	}
-	
+
 	public void saveJPEG() {
 		System.out.println("saveJPEG()1");
 		BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
 		Graphics g = img.getGraphics();
 		paint(g);
 		try {
-	        if (ImageIO.write(img, "jpg", new File("dir1/brad.jpg"))) {
-	        	System.out.println("OK");
-	        }else {
-	        	System.out.println("XX");
-	        }
+			if (ImageIO.write(img, "jpg", new File("dir1/brad.jpg"))) {
+				System.out.println("OK");
+			} else {
+				System.out.println("XX");
+			}
 			System.out.println("saveJPEG()2");
-	    } catch (IOException e) {
-	    	System.out.println(e);
-	    }
+		} catch (IOException e) {
+			System.out.println(e);
+		}
 	}
-	
+
 	public void saveLines() throws Exception {
-		try(ObjectOutputStream oout = 
-			new ObjectOutputStream(new FileOutputStream("dir1/brad.sign"))){
+		try (ObjectOutputStream oout = new ObjectOutputStream(new FileOutputStream("dir1/brad.sign"))) {
 			oout.writeObject(lines);
 			oout.flush();
 		}
 	}
-	
-	public void loadLines() throws Exception{
-		ObjectInputStream oin = 
-			new ObjectInputStream(new FileInputStream("dir1/brad.sign"));
+
+	public void loadLines() throws Exception {
+		ObjectInputStream oin = new ObjectInputStream(new FileInputStream("dir1/brad.sign"));
 		Object obj = oin.readObject();
-		lines = (LinkedList<Line>)obj;
+		lines = (LinkedList<Line>) obj;
 		oin.close();
-		
+
 		repaint();
 	}
-	
+
 }
 
 class Line implements Serializable {
 	private LinkedList<HashMap<String, Integer>> points;
 	private Color color;
 	private float width;
-	
+
 	public Line(Color color, float width) {
 		points = new LinkedList<>();
-		this.color = color; 
+		this.color = color;
 		this.width = width;
 	}
-	
+
 	public void addPoint(int x, int y) {
 		HashMap<String, Integer> point = new HashMap<>();
-		point.put("x", x); point.put("y", y);
+		point.put("x", x);
+		point.put("y", y);
 		points.add(point);
 	}
-	
+
 	public int getPointX(int index) {
 		return points.get(index).get("x");
 	}
+
 	public int getPointY(int index) {
 		return points.get(index).get("y");
 	}
-	public int getSize() {return points.size();}
+
+	public int getSize() {
+		return points.size();
+	}
 
 	public Color getColor() {
 		return color;
@@ -169,8 +198,5 @@ class Line implements Serializable {
 	public float getWidth() {
 		return width;
 	}
-	
-	
+
 }
-
-
